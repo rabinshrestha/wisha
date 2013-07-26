77b92a25838877b3d94abc9a773b5d87c70fab90
rabinshrestha <shrestha.rabin505@gmail.com>
2013-07-26 05:32:00 -0700

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -4,5 +4,5 @@ $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_port'] = 465;
 $config['smtp_user'] = 'shrestha.rabin505@gmail.com';
-$config['smtp_pass'] = 'saynocsG.O';
+$config['smtp_pass'] = '';
 $config['smtp_timeout'] = 5;
